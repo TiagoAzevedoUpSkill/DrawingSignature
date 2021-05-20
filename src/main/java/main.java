99560a99ae625f982121447aac0fc79bd5334783
Tiago Rafael Mina Azevedo <tiago.azevedo@servexternos.santander.pt>
2021-05-20 18:03:26 +0100

    Update to signature

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -29,18 +29,23 @@ public class main {
         System.out.println("Execution complete");
     }
 
+    int[] arrayX = {374,378,380,386,397,401,408,422,438,448,462,466,469,476,489,494,502,507,513,517};
+
+    int[] arrayY = {127,120,110,87,62,56,51,49,60,70,88,94,96,95,94,96,100,97,92,90};
+
     static void draw(WebDriver driver, String element) throws InterruptedException {
         Actions builder = new Actions(driver);
         Action drawAction = builder.moveToElement(driver.findElement(By.xpath(element))) //start points x axis and y axis.
                 .clickAndHold()
-                .moveByOffset(-30, 60) // 2nd points (x1,y1)
-                .moveByOffset(-60, -70)// 3rd points (x2,y2)
-                .moveByOffset(150, 60) // 2nd points (x1,y1)
-                .moveByOffset(-60, -70)// 3rd points (x2,y2)
-                .moveByOffset(-50, 100) // 2nd points (x1,y1)
-                .moveByOffset(-60, 200)// 3rd points (x2,y2)
-                .moveByOffset(150, -50) // 2nd points (x1,y1)
-                .moveByOffset(-60, -100)// 3rd points (x2,y2)
+                .moveByOffset(4, -7) // 2nd points (x1,y1)
+                .moveByOffset(2, -10)// 3rd points (x2,y2)
+                .moveByOffset(6, -30) // 2nd points (x1,y1)
+                .moveByOffset(11, -15)// 3rd points (x2,y2)
+                .moveByOffset(4, -8) // 2nd points (x1,y1)
+                .moveByOffset(7, -2)// 3rd points (x2,y2)
+                .moveByOffset(14, 11) // 2nd points (x1,y1)
+                .moveByOffset(16, 10)// 3rd points (x2,y2)
+
 //                .doubleClick()
                 .build();
         drawAction.perform();
